de7b07406729235a5fbd19b86f5fb53ff2a8f789
Osama <osama.khan@digidots.net>
2021-01-09 18:17:34 +0500

    API integration work , make some test api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\Api\CustomerController;
+use App\http\Controllers\Api\SubsPlanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/allcustomers', [CustomerController::class , 'index']);
+
+Route::get('/customer/{id}', [CustomerController::class , 'show']);
+
+Route::get('/allsubsplans', [SubsPlanController::class , 'index']);
