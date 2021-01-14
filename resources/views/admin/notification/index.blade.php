dcb3b16701771b7893aa343ddf9f1bc74de1af4d
Osama <osama.khan@digidots.net>
2021-01-14 15:52:50 +0500

    notification message fixes

--- resources/views/admin/notification/index.blade.php
+++ resources/views/admin/notification/index.blade.php
@@ -26,11 +26,11 @@
                                     <span aria-hidden="true">×</span>
                                 </button>
                             </div>
-                        @elseif($notifiydeletesuccess = Session::get('notifiydeletesuccess'))
+                        @elseif($notifiysendsuccess = Session::get('notifiysendsuccess'))
                             <div class="card-alert card gradient-45deg-green-teal">
                                 <div class="card-content white-text">
                                     <p>
-                                        <i class="material-icons">check</i>{{ $notifiydeletesuccess }}</p>
+                                        <i class="material-icons">check</i>{{ $notifiysendsuccess }}</p>
                                 </div>
                                 <button type="button" class="close white-text" data-dismiss="alert" aria-label="Close">
                                     <span aria-hidden="true">×</span>
