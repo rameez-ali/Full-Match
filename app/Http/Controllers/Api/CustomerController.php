de7a4ffa634838102fa66e628265433c88510070
Osama <osama.khan@digidots.net>
2021-01-22 18:49:36 +0500

    user update api fixes

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -117,8 +117,16 @@ class CustomerController extends Controller
         $request->id = $id;
 
         $response = $request->handleProfileUpdate();
-            dd($response);
-//        return redirect()->route('customer.index')->with('usereditsuccess','User Edit Successfully');
+
+        if ($response) {
+
+            return response()->json(['success' => true, 'status' => $this->successStatus, 'message' => 'Profile Updated.']);
+
+        } else {
+
+            return response()->json(['success' => false, 'status' => $this->HTTP_NOT_FOUND, 'message' => 'Profile Not Updated.']);
+        }
+
     }
 
     /**
