3a6beea291d353823c913eea4336b8fdaf75322a
Osama <osama.khan@digidots.net>
2021-02-01 12:26:48 +0500

    social login fixes 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -78,6 +79,16 @@ Route::group(['middleware' => ['auth']], function () {
 
 // Route::get('dropdownlist','DataController@getCountries');
 // Route::get('dropdownlist/getstates/{id}','DataController@getStates');
+});
+
+Route::get('/google-redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/google-callback', function () {
+    $user = Socialite::driver('google')->user();
+
+    return $user->token;
 });
 Route::get('/home', 'HomeController@index')->name('home');
 
