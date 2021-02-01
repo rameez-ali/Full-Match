d5c52c22e8a2c017b9c8b15a1359e61a7b902faa
rameez ali <rameezali1995@gmail.com>
2021-02-01 12:41:11 +0500

    sidenav updated

--- resources/views/admin/layouts/partials/sidenav.blade.php
+++ resources/views/admin/layouts/partials/sidenav.blade.php
@@ -26,37 +26,40 @@
         <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('notification.index') }}"><i class="material-icons dp48">notifications_active</i><span class="menu-title" data-i18n="User Profile">{{ __('customer.notification.notification') }}</span></a>
         </li>
 
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('Contactus-form.index') }}"><i class="material-icons">person_outline</i><span class="menu-title" data-i18n="User Profile">Contact Us</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('Contactus-form.index') }}"><i class="material-icons">add_location</i><span class="menu-title" data-i18n="User Profile">Contact Us</span></a>
         </li>
 
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('contact-form.index') }}"><i class="material-icons">person_outline</i><span class="menu-title" data-i18n="User Profile">Contact Queries</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('contact-form.index') }}"><i class="material-icons">question_answer
+                </i><span class="menu-title" data-i18n="User Profile">Contact Queries</span></a>
         </li>
 
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('category-form.index') }}"><i class="material-icons">person_outline</i><span class="menu-title" data-i18n="User Profile">Category</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('category-form.index') }}"><i class="material-icons">domain</i><span class="menu-title" data-i18n="User Profile">Category</span></a>
         </li>
 
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('genre-form.index') }}"><i class="material-icons">person_outline</i><span class="menu-title" data-i18n="User Profile">Genre</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('genre-form.index') }}"><i class="material-icons">video_library</i><span class="menu-title" data-i18n="User Profile">Genre</span></a>
         </li>
 
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('player-form.index') }}"><i class="material-icons">person_outline</i><span class="menu-title" data-i18n="User Profile">Players</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('player-form.index') }}"><i class="material-icons">directions_walk
+                </i><span class="menu-title" data-i18n="User Profile">Players</span></a>
         </li>
 
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('club-form.index') }}"><i class="material-icons">person_outline</i><span class="menu-title" data-i18n="User Profile">Club</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('club-form.index') }}"><i class="material-icons">border_all</i><span class="menu-title" data-i18n="User Profile">Club</span></a>
         </li>
 
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('league-form.index') }}"><i class="material-icons">person_outline</i><span class="menu-title" data-i18n="User Profile">League</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('league-form.index') }}"><i class="material-icons">event</i><span class="menu-title" data-i18n="User Profile">League</span></a>
         </li>
 
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('video-form.index') }}"><i class="material-icons">person_outline</i><span class="menu-title" data-i18n="User Profile">Videos</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('video-form.index') }}"><i class="material-icons">ondemand_video
+                </i><span class="menu-title" data-i18n="User Profile">Videos</span></a>
         </li>
 
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('slider-form.index') }}"><i class="material-icons">person_outline</i><span class="menu-title" data-i18n="User Profile">Category Slider</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('slider-form.index') }}"><i class="material-icons">slideshow</i><span class="menu-title" data-i18n="User Profile">Category Slider</span></a>
         </li>
 
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('banner-form.index') }}"><i class="material-icons">person_outline</i><span class="menu-title" data-i18n="User Profile">Advertisement Banner</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('banner-form.index') }}"><i class="material-icons">announcement</i><span class="menu-title" data-i18n="User Profile">Advertisement Banner</span></a>
         </li>
 
-        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('seasonpart-form.index') }}"><i class="material-icons">person_outline</i><span class="menu-title" data-i18n="User Profile">Season Part Sorting</span></a>
+        <li class="bold"><a class="waves-effect waves-cyan " href="{{ route('seasonpart-form.index') }}"><i class="material-icons">ac_unit</i><span class="menu-title" data-i18n="User Profile">Season Part Sorting</span></a>
         </li>
 
         <li  class="bold"><a class="waves-effect waves-cyan " href="{{ route('customer.logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="material-icons">keyboard_tab</i>{{ __('Logout') }}</a></li>
