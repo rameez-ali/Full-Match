7bf42a44194839e45bee3494598cb27ad8aae323
Osama <osama.khan@digidots.net>
2021-02-18 15:03:14 +0500

    notifiy work

--- app/Http/Requests/Notification/SendNotificationRequest.php
+++ app/Http/Requests/Notification/SendNotificationRequest.php
@@ -42,7 +42,7 @@ class SendNotificationRequest extends FormRequest
          }
 
 //        $tokenList = DeviceToken::pluck('token')->toArray();
-         dd($tokenList);
+//         dd($tokenList);
         $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
         // $tokenList[] = 'eCcZ-L2K49w:APA91bHyArn9JrTqLWl1vmd_3IgE-XIDEwKAFL5B5g3RVBrSfacFfvQrXuywNXZuE2WlplXBxkFxmHoZb5oSc1hZayy8XGght7j6AyC4cS7661ck9UHseayLNPcxopCX2nWdKSXKwB8D';
         // $tokenList[] = 'f0xnoyCn90orlDK8SLGX8N:APA91bHl1l8tO_GClxgjtjsXHbO_5viCxCKJ3dmLcYbzcCcE82wxymm3IVJV9dc2OpIRkfTWBM3frUQ5Q2ZdPi6LVtSnbPSp0I7Rk4DmSaagRfmkhnQ_uwHBH3i8S8atdtk4TsTOSb5H';
