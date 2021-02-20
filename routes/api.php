c47aeb80f4a8ed26896a926154cd7dd1bd3d0f69
Osama <osama.khan@digidots.net>
2021-02-20 16:44:47 +0500

    commiting

--- routes/api.php
+++ routes/api.php
@@ -77,14 +77,14 @@ Route::group([
     Route::post('socialLogin', [ApiAuthController::class , 'socialLogin']);
     Route::post('signup', [ApiAuthController::class , 'signup']);
 
-    Route::get('/google-redirect', [ApiAuthController::class , 'googleRedirect']);
-    Route::get('/google-callback', [ApiAuthController::class , 'googlecCallback']);
-
-    Route::get('/facebook-redirect', [ApiAuthController::class , 'facebookRedirect']);
-    Route::get('/facebook-callback', [ApiAuthController::class , 'facebookCallback']);
-
-    Route::get('/apple-redirect', [ApiAuthController::class , 'appleRedirect']);
-    Route::get('/apple-callback', [ApiAuthController::class , 'appleCallback']);
+//    Route::get('/google-redirect', [ApiAuthController::class , 'googleRedirect']);
+//    Route::get('/google-callback', [ApiAuthController::class , 'googlecCallback']);
+//just put these routes and functions
+//    Route::get('/facebook-redirect', [ApiAuthController::class , 'facebookRedirect']);
+//    Route::get('/facebook-callback', [ApiAuthController::class , 'facebookCallback']);
+//
+//    Route::get('/apple-redirect', [ApiAuthController::class , 'appleRedirect']);
+//    Route::get('/apple-callback', [ApiAuthController::class , 'appleCallback']);
 
 });
 
