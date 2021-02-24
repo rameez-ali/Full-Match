5621d073e2bb0759f331cce868c1fbfec14921b8
Osama <osama.khan@digidots.net>
2021-02-24 13:24:42 +0500

    reset password redirect fixes

---
+++ resources/views/pwdchange.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>FullMatch</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h3>FullMatch</h3>
+
+        <h2>Your Password Has Been Changed. </h2>
+
+    </body>
+</html>
