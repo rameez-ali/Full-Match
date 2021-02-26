940ab3f9db07f3d7a8d69910cc848b1cfc47d412
Osama <osama.khan@digidots.net>
2021-02-26 15:56:11 +0500

    Notification Send Fixes

--- app/Model/DeviceToken.php
+++ app/Model/DeviceToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Model;
 
+use App\customer;
 use Illuminate\Database\Eloquent\Model;
 
 class DeviceToken extends Model
@@ -9,4 +10,9 @@ class DeviceToken extends Model
     protected $fillable = [
         'device','token','user_id',
     ];
+
+    public function checknotify()
+    {
+        return $this->belongsTo(customer::class , 'user_id', 'user_id');
+    }
 }
