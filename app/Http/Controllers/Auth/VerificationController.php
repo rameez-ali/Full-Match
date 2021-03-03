67794c1088bd8b8fd037b5a3a6a7009e04b5b49e
Osama <osama.khan@digidots.net>
2021-03-03 12:17:21 +0500

    email verify work

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Http\Request;
 
 class VerificationController extends Controller
 {
@@ -39,4 +40,10 @@ class VerificationController extends Controller
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
+    public function verify(Request $request)
+    {
+        dd($request);
+    }
+
+
 }
