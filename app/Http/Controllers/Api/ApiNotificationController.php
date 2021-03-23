51cec4fe2fcdee4687983e526a7c0587de483c1b
Osama <osama.khan@digidots.net>
2021-03-23 19:02:27 +0500

    notification toggle fixes

--- app/Http/Controllers/Api/ApiNotificationController.php
+++ app/Http/Controllers/Api/ApiNotificationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\customer;
 use App\Http\Controllers\Controller;
+use App\Model\DeviceToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\User;
@@ -16,14 +17,14 @@ class ApiNotificationController extends Controller
 
     public function notifiOff(Request $request)
     {
-        $cust = customer::where('user_id',$request->user()->id)->First();
+        $cust = DeviceToken::where('user_id',$request->user()->id)->First();
         $cust->notify_status = 0;
         $cust->save();
         return response()->json(['success' => true, 'status' => $this->successStatus, 'message' => 'Notification OFF.']);
     }
     public function notifiOn(Request $request)
     {
-        $cust = customer::where('user_id',$request->user()->id)->First();
+        $cust = DeviceToken::where('user_id',$request->user()->id)->First();
         $cust->notify_status = 1;
         $cust->save();
         return response()->json(['success' => true, 'status' => $this->successStatus, 'message' => 'Notification ON.']);
